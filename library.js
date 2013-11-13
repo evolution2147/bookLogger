ce7ce76e056ae5671f3e8b5a2825c654fe1501cb
dchaides <dchaides@gmail.com>
2013-11-12 22:15:51 -0700

    added meteor method to return book description

--- library.js
+++ library.js
@@ -1,103 +1,51 @@
 var clearValues = function() {
-
     $('#name').val("").focus();
-
     $('#author').val("");
-
-    $('#pubdate').val(""); 
-
+    $('#pubdate').val("");
     $('#description').val("");
-
 }
-
-
-
-function updateId() {
-
-  var idVal = Math.floor((Math.random()*100+1));
-
-  return idVal.toString();
-
-}
-
-
 
 Library = new Meteor.Collection('library');
-
 if (Meteor.isClient) {
-
  Template.main.library = function(){
-
   return Library.find();
-
   }
 
-
-
 Template.main.events(
-
 {
-
   'click .add': function() {
-
-      //var bookID = updateId();
-
       var name = $('#name').val();
-
       var author = $('#author').val();
-
       var pubdate = $('#pubdate').val();
-
       var description = $('#description').val();
-
+      var checkval = 0;
       Library.insert({name:name,author:author,pubdate:pubdate, 
-
-        description:description});
-
+        description:description, check:checkval});
       clearValues();
-
-    }
-
-},
-
-  {
-
-  'click .deleteBook': function() 
-
+    },
+  'click .removeAll': function() 
     {
+     Meteor.call('removeA');
+    },
 
-     var bookID = document.getElementById("name").value;
-
-     Library.remove(bookID);
-
+    'click .destroy': function () {
+      Library.remove(this._id);
+    },
+    'click .bookCall': function() {
+      $('#myModal').modal(show);
     }
+  }
+  );
+}
 
+if (Meteor.isServer){
+  Meteor.methods({ 
+    removeA: function() {
+      Library.remove({}); 
   },
-
-  
-
+    bookInfo: function(_id)
   {
-
-    'click .removeAll': function () 
-
-    {
-
-      Library.remove({});
-
-    }
-
+    
+  }
   });
-
 }
-
-
-
-if (Meteor.isServer)
-{
-  
-  Meteor.methods({removeA: function() {
-
-  Library.remove({}) }});
-
-}
-
